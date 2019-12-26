ce23af25f6caf9f1409a47d8ef597333cd490caf
Manuel Pinto <hello@manuelpinto.in>
2019-12-26 10:56:41 +0530

    Only set spectrum profile if it doesn't exist
    The old method fails on newer android version

--- ramdisk/init.spectrum.sh
+++ ramdisk/init.spectrum.sh
@@ -3,6 +3,6 @@
 # Profile initialization script by nathanchance
 
 # If there is not a persist value, we need to set one
-if [ ! -f /data/property/persist.spectrum.profile ]; then
+if [ "`getprop persist.spectrum.profile`" == "" ]; then
     setprop persist.spectrum.profile 0
 fi
